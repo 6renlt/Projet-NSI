42359924da2b4e4041fe71af08fd32ebe210f565
6renlt <126596900+6renlt@users.noreply.github.com>
2023-03-28 10:54:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # Ceci est le fichier principal
+import pygame
+
+def jeu_beret():
+
+
+def map():
+
+
+def personnages():
+    
+
+def temps():
+    
+    
+def fin():
+    
+    
+def objet():
+    
+    
+    
+    
