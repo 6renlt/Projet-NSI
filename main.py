d4cf9727c4331426c060a8e477d8134d2484b9ac
GabinoMalvillo <126895340+GabinoMalvillo@users.noreply.github.com>
2023-03-28 11:31:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,23 @@ def map():
 
 
 def personnages():
+    personnage_test = pygame.Rect((360,240), (32,32))
+    pygame.key.set_repeat(1,20)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    personnage_test.move_ip((5,0))
+                elif event.key == pygame.K_UP:
+                    personnage_test.move_ip((0,-5))
+                elif event.key == pygame.K_LEFT:
+                    personnage_test.move_ip((-5,0))
+                elif event.key == pygame.K_DOWN:
+                    personnage_test.move_ip((0,5))
     
 
 def temps():
