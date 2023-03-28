83c6dd725f7a190105f7fefcd3e529133ad7483a
6renlt <126596900+6renlt@users.noreply.github.com>
2023-03-28 11:11:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # Ceci est le fichier principal
 import pygame
+from pygame.locals import *
+pygame.init()
 
 def jeu_beret():
+    screen = display.set_mode((1920,1080))
+    
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit ()
 
 
 def map():
